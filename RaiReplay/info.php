b3aacb7086a205a614f7b76907961014a88e083a
nixxo <c.nixxo@hush.ai>
2015-11-21 21:35:59 +0100

    minor fixes

--- RaiReplay/info.php
+++ RaiReplay/info.php
@@ -6,7 +6,7 @@
 # meta-date="2015-11-22"
 # meta-version="0.1"
 # meta-type="umsp"
-# meta-url="http://todo.web"
+# meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305"
 # meta-filename="RaiReplay.php"
 # meta-id="RaiReplay"
 # meta-basefirmware="*"
@@ -22,7 +22,7 @@ $pluginInfo = array(
 	'author' => 'nixxo',
 	'date' => '2015-11-22',
 	'version' => '0.1',
-	'url' => 'http://todo.web',
+	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
 	'id' => 'RaiReplay',
 	'thumb' => 'http://www.rai.tv/dl/RaiTV/2012/images/logo.png',
 	'art' => 'http://www.rai.tv/dl/RaiTV/2012/images/logo.png',
