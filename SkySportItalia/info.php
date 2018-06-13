bc4a755b25940953b3979fc93e72587dc462f109
nixxo <c.nixxo@gmail.com>
2018-06-13 21:48:13 +0200

    SkySport - first release

---
+++ SkySportItalia/info.php
@@ -0,0 +1,31 @@
+<?php
+
+# meta-name="Sky Sport"
+# meta-desc="Guarda i video di Sky Sport Italia."
+# meta-author="nixxo"
+# meta-date="2016-07-23"
+# meta-version="0.1"
+# meta-type="umsp"
+# meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305"
+# meta-filename="SkySportItalia.php"
+# meta-id="SkySportItalia"
+# meta-basefirmware="*"
+# meta-thumbnail="http://nst.sky.it/content/dam/skyit/loghi_videoportale/sport.png"
+# meta-rating="General"
+# meta-category="Television"
+
+global $pluginInfo;
+
+$pluginInfo = array(
+	'name' => 'Sky Sport',
+	'desc' => 'Guarda i video di Sky Sport Italia.',
+	'author' => 'nixxo',
+	'date' => '2016-07-23',
+	'version' => '0.1',
+	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
+	'id' => 'SkySportItalia',
+	'thumb' => 'http://nst.sky.it/content/dam/skyit/loghi_videoportale/sport.png',
+	'art' => 'http://nst.sky.it/content/dam/skyit/loghi_videoportale/sport.png',
+);
+
+?>
