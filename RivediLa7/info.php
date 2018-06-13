0532a2eccf517decb69113db08df6244bb31f88b
nixxo <c.nixxo@gmail.com>
2018-06-13 21:15:46 +0200

    la7 - updated logo
    updated logo and upped version

--- RivediLa7/info.php
+++ RivediLa7/info.php
@@ -3,14 +3,14 @@
 # meta-name="RivediLa7"
 # meta-desc="Rivedi la programmazione dell'ultima settimana di La7 direttamente dal tuo WDTV."
 # meta-author="nixxo"
-# meta-date="2016-07-21"
-# meta-version="0.4"
+# meta-date="2018-06-13"
+# meta-version="0.5"
 # meta-type="umsp"
 # meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9310"
 # meta-filename="RivediLa7.php"
 # meta-id="RivediLa7"
 # meta-basefirmware="*"
-# meta-thumbnail="http://s1.postimg.org/5zaz6vc7j/LA7_logo.png"
+# meta-thumbnail="http://www.la7.it/sites/default/files/la7_logo_0102_def.png"
 # meta-rating="General"
 # meta-category="Television"
 
@@ -20,12 +20,12 @@ $pluginInfo = array(
 	'name' => 'RivediLa7',
 	'desc' => 'Rivedi la programmazione dell\'ultima settimana di La7 direttamente dal tuo WDTV.',
 	'author' => 'nixxo',
-	'date' => '2016-07-21',
-	'version' => '0.4',
+	'date' => '2016-06-13',
+	'version' => '0.5',
 	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9310',
 	'id' => 'RivediLa7',
-	'thumb' => 'http://s1.postimg.org/5zaz6vc7j/LA7_logo.png',
-	'art' => 'http://s1.postimg.org/5zaz6vc7j/LA7_logo.png',
+	'thumb' => 'http://www.la7.it/sites/default/files/la7_logo_0102_def.png',
+	'art' => 'http://www.la7.it/sites/default/files/la7_logo_0102_def.png',
 );
 
 ?>
