f483eeaddd5b8bf28f99bdac12caa6be45613e51
nixxo <c.nixxo@gmail.com>
2019-06-10 17:31:54 +0200

    final update
    v1.0
    updated logo with wikipedia image

--- RivediLa7/info.php
+++ RivediLa7/info.php
@@ -3,14 +3,14 @@
 # meta-name="RivediLa7"
 # meta-desc="Rivedi la programmazione dell'ultima settimana di La7 direttamente dal tuo WDTV."
 # meta-author="nixxo"
-# meta-date="2018-06-13"
-# meta-version="0.5"
+# meta-date="2019-06-10"
+# meta-version="1.0"
 # meta-type="umsp"
 # meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9310"
 # meta-filename="RivediLa7.php"
 # meta-id="RivediLa7"
 # meta-basefirmware="*"
-# meta-thumbnail="http://www.la7.it/sites/default/files/la7_logo_0102_def.png"
+# meta-thumbnail="https://upload.wikimedia.org/wikipedia/commons/0/02/LA7_-_Logo_2011.svg"
 # meta-rating="General"
 # meta-category="Television"
 
@@ -20,12 +20,12 @@ $pluginInfo = array(
 	'name' => 'RivediLa7',
 	'desc' => 'Rivedi la programmazione dell\'ultima settimana di La7 direttamente dal tuo WDTV.',
 	'author' => 'nixxo',
-	'date' => '2016-06-13',
-	'version' => '0.5',
+	'date' => '2019-06-10',
+	'version' => '1.0',
 	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9310',
 	'id' => 'RivediLa7',
-	'thumb' => 'http://www.la7.it/sites/default/files/la7_logo_0102_def.png',
-	'art' => 'http://www.la7.it/sites/default/files/la7_logo_0102_def.png',
+	'thumb' => 'https://upload.wikimedia.org/wikipedia/commons/0/02/LA7_-_Logo_2011.svg',
+	'art' => 'https://upload.wikimedia.org/wikipedia/commons/0/02/LA7_-_Logo_2011.svg',
 );
 
 ?>
