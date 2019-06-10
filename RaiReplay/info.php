1ede69a11cc0ecfc3792a96a542ad9c31be3f4bf
nixxo <c.nixxo@gmail.com>
2019-06-10 17:31:46 +0200

    Final update
    v1.0
    updated logo with wikipedia image

--- RaiReplay/info.php
+++ RaiReplay/info.php
@@ -3,14 +3,14 @@
 # meta-name="Rai Play"
 # meta-desc="Rivedi la programmazione dell'ultima settimana dei canali Rai direttamente dal tuo WDTV."
 # meta-author="nixxo"
-# meta-date="2018-06-13"
-# meta-version="0.7"
+# meta-date="2019-06-10"
+# meta-version="1.0"
 # meta-type="umsp"
 # meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305"
 # meta-filename="RaiReplay.php"
 # meta-id="RaiReplay"
 # meta-basefirmware="*"
-# meta-thumbnail="http://www.rai.it/dl/components/img/nuovoLogoRaiPlayTrasparent.png"
+# meta-thumbnail="https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg"
 # meta-rating="General"
 # meta-category="Television"
 
@@ -20,12 +20,12 @@ $pluginInfo = array(
 	'name' => 'Rai Play',
 	'desc' => 'Rivedi la programmazione dell\'ultima settimana dei canali Rai direttamente dal tuo WDTV.',
 	'author' => 'nixxo',
-	'date' => '2018-06-13',
-	'version' => '0.7',
+	'date' => '2019-06-10',
+	'version' => '1.0',
 	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
 	'id' => 'RaiReplay',
-	'thumb' => 'http://www.rai.it/dl/components/img/nuovoLogoRaiPlayTrasparent.png',
-	'art' => 'http://www.rai.it/dl/components/img/nuovoLogoRaiPlayTrasparent.png',
+	'thumb' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
+	'art' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
 );
 
 ?>
