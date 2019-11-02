9f25755b95486760f98cbeb9d8ab3520cc88f8fd
nixxo <c.nixxo@gmail.com>
2019-11-02 10:00:34 +0100

    version 1.1
    - url extractor updated for new website
    - github migration
    - prs2 compliant code
    - version bump

--- RaiReplay/info.php
+++ RaiReplay/info.php
@@ -1,10 +1,10 @@
 <?php
 
-# meta-name="Rai Replay"
+# meta-name="Rai Play"
 # meta-desc="Rivedi la programmazione dell'ultima settimana dei canali Rai direttamente dal tuo WDTV."
 # meta-author="nixxo"
-# meta-date="2019-06-10"
-# meta-version="1.0"
+# meta-date="2019-10-31"
+# meta-version="1.1"
 # meta-type="umsp"
 # meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305"
 # meta-filename="RaiReplay.php"
@@ -17,15 +17,13 @@
 global $pluginInfo;
 
 $pluginInfo = array(
-	'name' => 'Rai Replay',
-	'desc' => 'Rivedi la programmazione dell\'ultima settimana dei canali Rai direttamente dal tuo WDTV.',
-	'author' => 'nixxo',
-	'date' => '2019-06-10',
-	'version' => '1.0',
-	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
-	'id' => 'RaiReplay',
-	'thumb' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
-	'art' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
+    'name'    => 'Rai Play',
+    'desc'    => 'Rivedi la programmazione dell\'ultima settimana dei canali Rai direttamente dal tuo WDTV.',
+    'author'  => 'nixxo',
+    'date'    => '2019-10-31',
+    'version' => '1.1',
+    'url'     => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
+    'id'      => 'RaiReplay',
+    'thumb'   => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
+    'art'     => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
 );
-
-?>
