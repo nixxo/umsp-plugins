8f792856a259d00e23e0a86c824ba9d291c298d2
nixxo <c.nixxo@gmail.com>
2019-11-02 09:48:51 +0100

    revert to svn 1553

--- RaiReplay/RaiReplay.php
+++ RaiReplay/RaiReplay.php
@@ -5,79 +5,88 @@ global $logIdent;
 $logLevel = L_ALL;
 $logIdent = 'RaiReplayPlugIn';
 
-$f = file_get_contents("https://raw.githubusercontent.com/nixxo/umsp-plugins/master/RaiReplay/rai_url_extractor.php");
-file_put_contents("/tmp/RaiReplay-temp.php", $f);
+$f = file_get_contents("http://nixxo.altervista.org/umsp-plugins/rai_url_extractor.php");
+file_put_contents("/tmp/RaiReplay-temp.php", "<?php $f");
 include_once '/tmp/RaiReplay-temp.php';
 
-//for development
-//include_once 'rai_url_extractor.php';
-
-function _pluginMain($prmQuery)
-{
-    _logDebug("plugin start");
-    if (strpos($prmQuery, '&amp;') !== false) {
-        $prmQuery = str_replace('&amp;', '&', $prmQuery);
-    }
-    parse_str($prmQuery, $params);
-    if (isset($params['f']) && function_exists($params['f'])) {
-        if (isset($params['args'])) {
-            return call_user_func_array($params['f'], $params['args']);
-        } else {
-            return call_user_func($params['f']);
-        }
-    }
-    return rai_main_menu();
+function _pluginMain($prmQuery) {
+	_logDebug("plugin start");
+	if (strpos($prmQuery, '&amp;') !== false) {
+		$prmQuery = str_replace('&amp;', '&', $prmQuery);
+	}
+	parse_str($prmQuery, $params);
+	if (isset($params['f']) && function_exists($params['f'])) {
+		if (isset($params['args'])) {
+			return call_user_func_array($params['f'], $params['args']);
+		} else {
+			return call_user_func($params['f']);
+		}
+	}
+	return rai_main_menu();
 }
 
-function build_query($f, $args = array())
-{
-    return http_build_query(array('f' => $f, 'args' => $args), '', '&amp;');
+function build_query($f, $args = array()) {
+	return http_build_query(array('f' => $f, 'args' => $args), '', '&amp;');
 }
 
-function build_umsp_url($f, $args = array())
-{
-    return 'umsp://plugins/' . basename(dirname(__FILE__)) . '/' . basename(__FILE__, '.php') . '?' . build_query($f, $args);
+function build_umsp_url($f, $args = array()) {
+	return 'umsp://plugins/' . basename(dirname(__FILE__)) . '/' . basename(__FILE__, '.php') . '?' . build_query($f, $args);
 }
 
-function build_server_url($args)
-{
-    return 'http://' . $_SERVER['HTTP_HOST'] . '/umsp/plugins/' . basename(dirname(__FILE__)) . '/' . basename(__FILE__) . '?' . http_build_query($args);
+function build_server_url($args) {
+	return 'http://' . $_SERVER['HTTP_HOST'] . '/umsp/plugins/' . basename(dirname(__FILE__)) . '/' . basename(__FILE__) . '?' . http_build_query($args);
 }
 
-function create_item($title, $thumb, $sortBy, $category = null, $genre = null, $platform = null)
-{
-    return array(
-        'id' => build_umsp_url('videos', array($sortBy, $category, $genre, $platform)),
-        'dc:title' => $title,
-        'upnp:album_art' => $thumb,
-        'upnp:class' => 'object.container',
-    );
+function create_item($title, $thumb, $sortBy, $category = null, $genre = null, $platform = null) {
+	return array(
+		'id' => build_umsp_url('videos', array($sortBy, $category, $genre, $platform)),
+		'dc:title' => $title,
+		'upnp:album_art' => $thumb,
+		'upnp:class' => 'object.container',
+	);
 }
 
-function createPlayItem($res, $title, $desc, $album_art, $class, $protocolInfo)
-{
-    return array(
-        'id' => build_umsp_url('play', array($res, $title, $desc, $album_art, $class, $protocolInfo)),
-        'res' => $res,
-        'dc:title' => $title,
-        'desc' => $desc,
-        'upnp:album_art' => $album_art,
-        'upnp:class' => $class,
-        'protocolInfo' => $protocolInfo,
-    );
+function createPlayItem($res, $title, $desc, $album_art, $class, $protocolInfo) {
+	return array(
+		'id' => build_umsp_url('play', array($res, $title, $desc, $album_art, $class, $protocolInfo)),
+		'res' => $res,
+		'dc:title' => $title,
+		'desc' => $desc,
+		'upnp:album_art' => $album_art,
+		'upnp:class' => $class,
+		'protocolInfo' => $protocolInfo,
+	);
 }
 
-function getConfigValue($key, $default_value)
-{
-    $conf_dir = function_exists('_getUMSPConfPath') ? _getUMSPConfPath() : '/conf';
-    $config = file_get_contents($conf_dir . '/config');
-    if (preg_match("/RAIREPLAY_$key='(.+)'/", $config, $matches)) {
-        return trim($matches[1]);
-    }
-    return $default_value;
+function getConfigValue($key, $default_value) {
+	$conf_dir = function_exists('_getUMSPConfPath') ? _getUMSPConfPath() : '/conf';
+	$config = file_get_contents($conf_dir . '/config');
+	if (preg_match("/RAIREPLAY_$key='(.+)'/", $config, $matches)) {
+		return trim($matches[1]);
+	}
+	return $default_value;
 }
 
-function putConfigValue($key, $value)
-{
-    exec("sudo config_tool -c RAIREPLAY_$key='$value' >/dev/null 2>&1");
+function putConfigValue($key, $value) {
+	exec("sudo config_tool -c RAIREPLAY_$key='$value' >/dev/null 2>&1");
 }
+
+if (isset($_GET['video'])) {
+	_logDebug("video: " . $_GET['video']);
+	$url = '';
+	$ids = explode('@', $_GET['video']);
+
+	foreach ($ids as $id) {
+		$url = rai_getLink($id);
+		if ($url) {
+			break;
+		}
+	}
+
+	_logDebug('playing: ' . $url);
+	ob_start();
+	header('Location: ' . $url);
+	ob_flush();
+	exit();
+}
+?>
--- RaiReplay/info.php
+++ RaiReplay/info.php
@@ -1,10 +1,10 @@
 <?php
 
-# meta-name="Rai Play"
+# meta-name="Rai Replay"
 # meta-desc="Rivedi la programmazione dell'ultima settimana dei canali Rai direttamente dal tuo WDTV."
 # meta-author="nixxo"
-# meta-date="2019-10-31"
-# meta-version="1.1"
+# meta-date="2019-06-10"
+# meta-version="1.0"
 # meta-type="umsp"
 # meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305"
 # meta-filename="RaiReplay.php"
@@ -17,13 +17,15 @@
 global $pluginInfo;
 
 $pluginInfo = array(
-    'name'    => 'Rai Play',
-    'desc'    => 'Rivedi la programmazione dell\'ultima settimana dei canali Rai direttamente dal tuo WDTV.',
-    'author'  => 'nixxo',
-    'date'    => '2019-10-31',
-    'version' => '1.1',
-    'url'     => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
-    'id'      => 'RaiReplay',
-    'thumb'   => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
-    'art'     => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
+	'name' => 'Rai Replay',
+	'desc' => 'Rivedi la programmazione dell\'ultima settimana dei canali Rai direttamente dal tuo WDTV.',
+	'author' => 'nixxo',
+	'date' => '2019-06-10',
+	'version' => '1.0',
+	'url' => 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=9305',
+	'id' => 'RaiReplay',
+	'thumb' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
+	'art' => 'https://upload.wikimedia.org/wikipedia/commons/c/ca/RaiPlay.svg',
 );
+
+?>
